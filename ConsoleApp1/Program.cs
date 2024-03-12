6acfa6d05f99844200682e8f6c5b31d6daf51946
0Goktug0 <s28439@pjwstk.edu.pl>
2024-03-12 13:01:09 +0100

    Initial project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿static double GetAverage(int[] numbers)
+{
+    double sum = 0.0;
+    foreach (var number in numbers)
+    {
+        sum += number;
+    }
+
+    return sum / numbers.Length;
+}
+
+int[] numbers = { 1, 2, 3, 4 };
+double average = GetAverage(numbers);
+Console.WriteLine(average);
